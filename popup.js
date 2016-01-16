71ea96f8596b4fd5b6553f671035ec938c2a1544
abhavedula <abhaved@seas.upenn.edu>
2016-01-16 16:41:06 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded',function() {
 	chrome.windows.getCurrent(null, function(w) {
 		var text = w.getSelection().toString();
-	})
+	});
 
 	document.getElementById("text").innerHTML = text;
 
 
-}
+});
