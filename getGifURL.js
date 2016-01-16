bcf081229dc2453391dd3917538e918089815790
Victoria Huang <vichuang@seas.upenn.edu>
2016-01-16 15:40:20 -0500

    started function

---
+++ getGifURL.js
@@ -0,0 +1,17 @@
+var apiKey = "c6e9079c85789eaf788aec678392451526d985f3"
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://gateway-a.watsonplatform.net/", false);
+xhr.send();
+console.log(xhr.status);
+console.log(xhr.statusText);
+
+function getGifURL(text){
+	
+	
+	
+	
+	
+	
+	// return URL
+}
