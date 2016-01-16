72142e13fdd5edaa7afbf16709725ee5fe1a54b8
abhavedula <abhaved@seas.upenn.edu>
2016-01-16 18:46:07 -0500

    added function to copy to clipboard

--- popup.js
+++ popup.js
@@ -9,4 +9,16 @@ document.addEventListener('DOMContentLoaded',function() {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     document.getElementById("text").innerHTML = request.selection;
+    copyToClipboard(request.selection) 
 });
+
+function copyToClipboard(text) {
+  const input = document.createElement('input');
+  input.style.position = 'fixed';
+  input.style.opacity = 0;
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('Copy');
+  document.body.removeChild(input);
+};
