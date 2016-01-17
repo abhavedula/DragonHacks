8f4d9cb97f5c2bc90063e4748f39fcaccfc0beac
ssark <ssark@seas.upenn.edu>
2016-01-16 20:11:45 -0500

    Update getGifURL.js

--- getGifURL.js
+++ getGifURL.js
@@ -1,17 +1,25 @@
-var apiKey = "c6e9079c85789eaf788aec678392451526d985f3"
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript">
 
+var apikey = "c6e9079c85789eaf788aec678392451526d985f3";
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://gateway-a.watsonplatform.net/", false);
-xhr.send();
-console.log(xhr.status);
-console.log(xhr.statusText);
 
-function getGifURL(text){
-	
-	
-	
-	
-	
-	
-	// return URL
+var apiCall = "https://gateway-a.watsonplatform.net/calls/text/TextGetRankedConcepts?apikey=";
+var outputMode = "json";
+var maxRetrieve = "3";
+
+function getURL(text) {
+
+xhr.open("GET", apiCall + apikey + "&text=" + text + "&maxRetrieve=" + maxRetrieve + "&outputMode=" + outputMode, false);
+
+xhr.send();
+
+
 }
+
+</script>
+</head>
+<body></body>
+</html>
