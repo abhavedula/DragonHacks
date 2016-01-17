5670016b3097cd2ebfc8430413510497641ddb2d
ssark <ssark@seas.upenn.edu>
2016-01-16 22:28:04 -0500

    Update getGifURL.js

--- getGifURL.js
+++ getGifURL.js
@@ -6,18 +6,26 @@
 var apikey = "c6e9079c85789eaf788aec678392451526d985f3";
 var xhr = new XMLHttpRequest();
 
-var apiCall = "https://gateway-a.watsonplatform.net/calls/text/TextGetRankedConcepts?apikey=";
-var outputMode = "json";
+var apiCall = "https://gateway-a.watsonplatform.net/calls/text/TextGetRankedTaxonomy?apikey=";
+//var outputMode = "xml";
 var maxRetrieve = "3";
 
 function getURL(text) {
-
-xhr.open("GET", apiCall + apikey + "&text=" + text + "&maxRetrieve=" + maxRetrieve + "&outputMode=" + outputMode, false);
+xhr.open("GET", apiCall + apikey + "&text=" + text.replace(" ","%20") + "&maxRetrieve=" + maxRetrieve, false);
+//xhr.setRequestHeader('Content-Type', 'text/xml');
 
 xhr.send();
 
+var apiResult = xhr.responseXML;
+//var parsed = (apiResult.childNodes['0'].textContent);
+var parse = apiResult.getElementsByTagName("label");
+var key = parse[0].textContent.split("/");
+document.write(key);
 
+return key;
 }
+
+getURL("My favorite brands are BMW and Ferrari");
 
 </script>
 </head>
